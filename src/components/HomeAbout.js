527b1ff2602acd0732c8e66110721121c66bd65e
pizgo <k.polrolniczak@gmail.com>
2021-07-09 15:00:09 +0200

    about section

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Decoration from "../assets/Decoration.svg";
+import Signature from "../assets/Signature.svg";
+import People from "../assets/People.jpg";
 
 const HomeAbout = () => {
 
     return (
 
-        <div>To jest o nas</div>
+        <div className="container">
+            <section className="about">
+                <div className="about__text">
+                    <p className="about__header">
+                        O nas
+                    </p>
+                    <img src={Decoration} id="decoration" className="about__decorationImg"/>
+                    <p className="about__desc">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consectetur cum expedita id impedit inventore laudantium pariatur quia quos sapiente?
+                    </p>
+                    <img src={Signature} className="about__signature"/>
+                </div>
+                <div className="about__img">
+                    <img src={People} className="about__img-pic"/>
+                </div>
+            </section>
+        </div>
     )
 }
 
