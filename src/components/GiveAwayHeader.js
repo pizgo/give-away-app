d386020beb47102180500a20aefa96cfe625b9ba
KPolrolniczak <k.polrolniczak@gmail.com>
2021-08-29 13:36:24 +0200

    GIve Away header finished

--- src/components/GiveAwayHeader.js
+++ src/components/GiveAwayHeader.js
@@ -8,9 +8,9 @@ const GiveAwayHeader = () => {
 
         <div className="container">
             <section className="gaHeader">
-                <div className="gaHeader__imgBox">
-                <img src={FormHeroImage} alt="" className="gaHeader__imgBox-img"/>
-                </div>
+                {/*<div className="gaHeader__imgBox">*/}
+                <img src={FormHeroImage} alt="" className="gaHeader__img"/>
+                {/*</div>*/}
 
                 <div className="gaHeader__container">
                     <p className="gaHeader__text1">
@@ -20,22 +20,28 @@ const GiveAwayHeader = () => {
                     <p className="gaHeader__text2">
                         Wystarczą cztery proste kroki:</p>
                     <div className="gaHeader__step">
-                        <div className="gaHeader__step-box">
+                        <div className="gaHeader__step__box">
                             <p className="gaHeader__step-number">1</p>
-                            <p className="gaHeader__step-text">Wybierz rzeczy</p>
+                            <p className="gaHeader__step-text">Wybierz <br/>rzeczy</p>
                         </div>
-                        <div className="gaHeader__steps-box">
+                        <div className="gaHeader__step__box">
                             <p className="gaHeader__step-number">2</p>
-                            <p className="gaHeader__step-text">Spakuj je w worki</p>
+                            <p className="gaHeader__step-text">Spakuj je <br/>w worki</p>
                         </div>
-                        <div className="gaHeader__steps-box">
+                        <div className="gaHeader__step__box">
                             <p className="gaHeader__step-number">3</p>
-                            <p className="gaHeader__step-text">Wybierz fundację</p>
+                            <p className="gaHeader__step-text">Wybierz <br/>fundację</p>
                         </div>
-                        <div className="gaHeader__steps-box">
+                        <div className="gaHeader__step__box">
                             <p className="gaHeader__step-number">4</p>
-                            <p className="gaHeader__step-text">Zamów kuriera</p>
+                            <p className="gaHeader__step-text">Zamów <br/>kuriera</p>
                         </div>
+                    </div>
+                    <div className="gaHeader__square">
+                        <div className="gaHeader__square-item"></div>
+                        <div className="gaHeader__square-item"></div>
+                        <div className="gaHeader__square-item"></div>
+                        <div className="gaHeader__square-item"></div>
 
                     </div>
 
