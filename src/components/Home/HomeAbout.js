6996a36abab0134d14e513ec31252a0d2b66cd97
KPolrolniczak <k.polrolniczak@gmail.com>
2021-12-16 11:16:50 +0100

    finished rwd for about section

--- src/components/Home/HomeAbout.js
+++ src/components/Home/HomeAbout.js
@@ -10,20 +10,19 @@ const HomeAbout = () => {
         <div className="container">
             <section className="about">
                 <div className="about__box1">
-                    <div className="about__box1-text">
-                        <p id="section2" className="about__title">
+                    <div className="about__box1-textContainer">
+                        <p id="section2" className="about__box1-title">
                            O nas
                         </p>
                         <img src={Decoration} alt="" id="decoration" className="about__decorationImg"/>
-                        <p className="about__desc">
+                        <p className="about__box1-desc">
                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consectetur cum expedita id impedit inventore laudantium pariatur quia quos sapiente?
                          </p>
                     </div>
                     <img src={Signature} alt="" className="about__box1-img"/>
-
                 </div>
                 <div className="about__box2">
-                    <img src={People} alt="" className="about__img-pic"/>
+                    {/*<img src={People} alt="" className="about__img-pic"/>*/}
                 </div>
 
             </section>
